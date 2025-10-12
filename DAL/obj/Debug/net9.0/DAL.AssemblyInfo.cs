5514e8771227806418698484990b4d7088764ccd
Timmy Dev <joshephmartinez2007@gmail.com>
2025-10-12 11:33:28 -0500

    entity Producto added

--- DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad14591651b509eabc4fa5a818498c2b8a8d04a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b80e33b3388b6773e45442613b56328309a440d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
