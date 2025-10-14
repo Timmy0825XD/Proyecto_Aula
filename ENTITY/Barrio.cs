5dcaeb6a87437aa1eab9806111c7d256fe78d1b3
Timmy Dev <joshephmartinez2007@gmail.com>
2025-10-13 23:08:37 -0500

    Correccion clase Barrio

--- ENTITY/Barrio.cs
+++ ENTITY/Barrio.cs
@@ -2,6 +2,8 @@
 {
     public class Barrio
     {
-
+        public int IdBarrio { get; set; }
+        public string NombreBarrio { get; set; }
+        public Ciudad Ciudad { get; set; }
     }
 }
